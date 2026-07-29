0523f5da7bb3944ffeba69d67577137c864d0ad2
Mel Young <mel@mlyoung.cool>
2026-07-29 23:00:35 +1000

    cleanup

--- classify_evaluate.py
+++ classify_evaluate.py
@@ -6,19 +6,11 @@ import pickle
 
 from IPython import embed
 import rich
-from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.linear_model import LogisticRegressionCV, LinearRegression
-from sklearn.metrics import accuracy_score, classification_report, log_loss
-from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
-from sklearn.decomposition import PCA
 import sys
 import requests
+
 
 def load(url: str):
     print("Loading models...")
@@ -39,34 +31,10 @@ def load(url: str):
     prediction = classifier.predict_proba(features)
     ham_prob = prediction[0][0] * 100.0
     spam_prob = prediction[0][1] * 100.0
-    rich.print(f"[bold]Prediction:[/bold]\n[green]{ham_prob:.4f}% ham[/green]\n[red]{spam_prob:.4f}% spam[/red]")
-
-    # for name, classifier in classifiers.items():
-    #     print(f"Fitting classifier {name}...")
-    #     classifier.fit(X_train_vect, y_train)
-    #
-    #     print("Predicting...")
-    #     y_pred = classifier.predict(X_test_vect)
-    #     report = classification_report(y_test, y_pred)
-    #
-    #     rich.print(f"[bold red]Classification report for {name}:[/bold red]\n{report}")
-    #     print()
-    #
-    #     with open(f"data/classifier_{type(classifier).__name__}.dat", "wb") as f:
-    #         pickle.dump(classifier, f)
-    #
-    #     if isinstance(classifier, DecisionTreeClassifier):
-    #         with open("/tmp/tree.dot", "w") as f:
-    #             tree.export_graphviz(
-    #                 classifier,
-    #                 out_file=f,
-    #                 feature_names=vectorizer.get_feature_names_out(),
-    #                 class_names=classifier.classes_,
-    #                 filled=True,
-    #             )
-    #
-    #     if isinstance(classifier, MultinomialNB):
-    #         important_features(vectorizer, classifier, 20)
+    rich.print(
+        f"[bold]Prediction:[/bold]\n[green]{ham_prob:.4f}% ham[/green]\n[red]{
+            spam_prob:.4f}% spam[/red]"
+    )
 
 
 if __name__ == "__main__":
