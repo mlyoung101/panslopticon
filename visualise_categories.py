c2db421941afe1c1b0ebd17a28dfa10e43bf8c36
Mel Young <mel@mlyoung.cool>
2026-07-30 01:39:38 +1000

    add category visualiser

---
+++ visualise_categories.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2026 Mel Young.
+#
+# This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL
+# was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+import sqlite3
+import matplotlib.pyplot as plt
+
+import numpy as np
+import pandas as pd
+import rich
+from IPython import embed
+import seaborn as sns
+
+
+def main() -> pd.DataFrame:
+    print("Loading data...")
+    conn = sqlite3.connect("data/panslop.db")
+    spam = pd.read_sql_query("SELECT * FROM slop", conn)
+    ham = pd.read_sql_query("SELECT * FROM ham", conn)
+    conn.close()
+
+    sns.set(style="darkgrid")
+    spam["origin_src"] = spam["origin_src"].str.replace("tag-", "")
+    ham["origin_src"] = ham["origin_src"].str.replace("tag-", "")
+
+    # fucking insane
+    # https://stackoverflow.com/a/46624802/5007892
+    sns.countplot(
+        spam, x="origin_src", order=spam["origin_src"].value_counts().index
+    ).set(title="Spam")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
