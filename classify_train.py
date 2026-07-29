28cf164a289644fce9267cf60c43ef4a752b7f2b
Mel Young <mel@mlyoung.cool>
2026-07-29 22:10:47 +1000

    PCA and some other things

--- classify_train.py
+++ classify_train.py
@@ -12,7 +12,7 @@ import rich
 from IPython import embed
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.linear_model import LogisticRegressionCV
+from sklearn.linear_model import LogisticRegressionCV, LinearRegression
 from sklearn.metrics import accuracy_score, classification_report, log_loss
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
@@ -20,6 +20,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import tree
+from sklearn.decomposition import PCA
 
 # based on: https://github.com/nadinejackson1/text-classification-naive-bayes/blob/main/main.ipynb
 
@@ -27,10 +28,10 @@ from sklearn import tree
 def load_data() -> pd.DataFrame:
     conn = sqlite3.connect("data/panslop.db")
     spam = pd.read_sql_query(
-        "SELECT text FROM full_text ORDER BY RANDOM() LIMIT 6000", conn
+        "SELECT text FROM full_text ORDER BY RANDOM() LIMIT 8000", conn
     )
     ham = pd.read_sql_query(
-        "SELECT text FROM ham_full_text ORDER BY RANDOM() LIMIT 6000", conn
+        "SELECT text FROM ham_full_text ORDER BY RANDOM() LIMIT 8000", conn
     )
     conn.close()
 
@@ -40,6 +41,30 @@ def load_data() -> pd.DataFrame:
     return pd.concat([spam, ham], ignore_index=True)
 
 
+# https://stackoverflow.com/a/50810751/5007892
+def important_features(vectorizer, classifier, n=20):
+    class_labels = classifier.classes_
+    feature_names = vectorizer.get_feature_names_out()
+
+    topn_class1 = sorted(
+        zip(classifier.feature_count_[0], feature_names), reverse=True
+    )[:n]
+    topn_class2 = sorted(
+        zip(classifier.feature_count_[1], feature_names), reverse=True
+    )[:n]
+
+    print("Top ham features")
+
+    for coef, feat in topn_class1:
+        print(class_labels[0], coef, feat)
+
+    print("-----------------------------------------")
+    print("Top spam features")
+
+    for coef, feat in topn_class2:
+        print(class_labels[1], coef, feat)
+
+
 def train():
     print("Loading data...")
     data: pd.DataFrame = load_data().sample(frac=1)
@@ -47,24 +72,52 @@ def train():
 
     X = data["text"]
     y = data["label"]
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
     print("Vectorising...")
-    vectorizer = CountVectorizer(stop_words="english", max_features=10000)
+    vectorizer = TfidfVectorizer(stop_words="english", max_features=10000)
     X_train_vect = vectorizer.fit_transform(X_train)
     X_test_vect = vectorizer.transform(X_test)
 
+    # PCA
+    print("Doing PCA")
+    fig = plt.figure(1, figsize=(8, 6))
+    ax = fig.add_subplot(111)
+    X_reduced = PCA(n_components=2).fit_transform(X_train_vect)
+
+    scatter = ax.scatter(
+        X_reduced[:, 0],
+        X_reduced[:, 1],
+        c=[0 if x == "spam" else 1 for x in y_train],
+        s=40,
+    )
+
+    ax.set(
+        title="First principal components",
+        xlabel="1st Principal Component",
+        ylabel="2nd Principal Component",
+    )
+    ax.xaxis.set_ticklabels([])
+    ax.yaxis.set_ticklabels([])
+
+    # Add a legend
+    legend1 = ax.legend(
+        scatter.legend_elements()[0],
+        ["spam", "ham"],
+        loc="upper right",
+        title="Classes",
+    )
+    ax.add_artist(legend1)
+
+    plt.show()
+
     # based on https://scikit-learn.org/stable/auto_examples/classification/plot_classifier_comparison.html
     classifiers = {
-        "Decision Tree": DecisionTreeClassifier(max_depth=5),
         "Naive Bayes": MultinomialNB(),
+        "Decision Tree": DecisionTreeClassifier(max_depth=5),
         "KNN": KNeighborsClassifier(5),
-        "Linear SVM": SVC(kernel="linear", C=0.025, random_state=42),
-        "Random Forest": RandomForestClassifier(
-            max_depth=5, n_estimators=10, max_features=5, random_state=42
-        ),
+        # "Linear SVM": SVC(kernel="linear", C=0.025),
+        "Random Forest": RandomForestClassifier(max_depth=5, n_estimators=15),
         # "Logistic Regression": LogisticRegressionCV(
         #     Cs=np.logspace(-6, 6, 101),
         #     cv=10,
@@ -83,24 +136,24 @@ def train():
         y_pred = classifier.predict(X_test_vect)
         report = classification_report(y_test, y_pred)
 
-        rich.print(
-            f"[bold red]Classification report for {name}:[/bold red]\n[lime]{
-                report
-            }[/lime]"
-        )
+        rich.print(f"[bold red]Classification report for {name}:[/bold red]\n{report}")
         print()
 
-        if isinstance(classifier, DecisionTreeClassifier):
-            tree.plot_tree(classifier, class_names=True)
-            plt.show()
+        with open(f"data/classifier_{type(classifier)}.dat", "wb") as f:
+            pickle.dump(classifier, f)
 
-    # print("SPAM most salient:")
-    # spam_class_prob_sorted = classifier.feature_log_prob_[0, :].argsort()[::-1]
-    # print(np.take(vectorizer.get_feature_names_out(), spam_class_prob_sorted[:10]))
-    #
-    # print("HAM most salient:")
-    # ham_class_prob_sorted = classifier.feature_log_prob_[1, :].argsort()[::-1]
-    # print(np.take(vectorizer.get_feature_names_out(), ham_class_prob_sorted[:10]))
+        if isinstance(classifier, DecisionTreeClassifier):
+            with open("/tmp/tree.dot", "w") as f:
+                tree.export_graphviz(
+                    classifier,
+                    out_file=f,
+                    feature_names=vectorizer.get_feature_names_out(),
+                    class_names=classifier.classes_,
+                    filled=True,
+                )
+
+        if isinstance(classifier, MultinomialNB):
+            important_features(vectorizer, classifier, 20)
 
 
 if __name__ == "__main__":
